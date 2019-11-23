31f0e21e0c468158cd2e303de69a54507403ca7d
Raymond Daugherty <raymond.daugherty@outlook.com>
2019-11-22 20:02:32 -0500

    [file] CodeCademys Part 2: Functions initial commit

---
+++ 02. Intro to Functions/02. What is a Function.py
@@ -0,0 +1,12 @@
+#Functions are basically just containers of code. Instead of writing the code repeatedly (which can lead to typos and mistakes), you simply write the code into a function, and call it when needed.
+#  
+def sing_song():
+  print("You may say I'm a dreamer")
+  print("But I'm not the only one")
+  print("I hope some day you'll join us")
+  print("And the world will be as one")
+  
+# call sing_song() below:
+sing_song()
+# We can call it as many times as we want.
+sing_song()
