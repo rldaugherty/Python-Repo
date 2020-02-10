a26bd14cbaf45af35e26c136968b13fd7d80b7b4
Raymond Daugherty <raymond.daugherty@outlook.com>
2020-02-10 16:01:46 -0500

    [file] Add all true files from host to repo

---
+++ 02. Intro to Functions/05. Writing a Function - Copy.py
@@ -0,0 +1,6 @@
+#You define functions by using the "def" syntax before the function name. This tells Python that you are creatinbg a function.
+#This function only prints "This page is loading..." to the use when it is called using the "loading_screen()" syntax.
+def loading_screen():
+    print("This page is loading...")
+
+loading_screen()
